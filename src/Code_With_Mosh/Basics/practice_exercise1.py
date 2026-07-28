e9d616ae956e2c8451a24588e2dbcef8cfd7b283
imohitparkash <imohitparkash1@gmail.com>
2026-07-28 19:59:52 +0530

    Building strong fundamentals

---
+++ src/Code_With_Mosh/Basics/practice_exercise1.py
@@ -0,0 +1,3 @@
+full_name='john smith'
+age=20
+is_new= True
---
+++ src/Code_With_Mosh/Basics/practice_exercise3.py
@@ -0,0 +1,3 @@
+weight_in_pound=input('weight in pound? ')
+weight_in_kg=int(weight_in_pound)*0.454
+print('your weight in kg is '+str(weight_in_kg))
