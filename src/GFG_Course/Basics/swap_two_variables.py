f3efb04d21e6a353492900b5ee29417614a617cc
imohitparkash <imohitparkash1@gmail.com>
2026-09-01 22:55:29 +0530

    add swap two variables

---
+++ src/GFG_Course/Basics/swap_two_variables.py
@@ -0,0 +1,21 @@
+x=100
+y=200
+swap=x+y
+y=swap-y
+x=swap-x
+print(x)
+print(y)
+#for string
+a="hey"
+b="namaste"
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+#using comma
+i=5
+j=6
+i,j=j,i
+print(i)
+print(j)
