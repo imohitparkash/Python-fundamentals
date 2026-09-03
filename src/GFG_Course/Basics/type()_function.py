dcb0dcf9ed6b03d43fd778f4793b1224736eff12
imohitparkash <imohitparkash1@gmail.com>
2026-09-03 13:28:50 +0530

    add type function

---
+++ src/GFG_Course/Basics/type()_function.py
@@ -0,0 +1,26 @@
+#numerical type
+a=10
+print(type(a))
+b=10.5
+print(type(b))
+c=2+4j
+print(type(c))
+
+#bool or none type
+k=True
+print(type(k))
+l=None
+print(type(l)) 
+
+#Sequence types
+str="gfg"#string
+print(type(str))
+l=[10,20,30]#list which is mutable 
+print(type(l))
+t=(10,20,30) #tupe not mutable
+print(type(t))
+s={10,20,30}#set not repeatable
+print(type(s))
+d={10:"gfg",20:"ide"}#Dict 
+print(type(d))
+
