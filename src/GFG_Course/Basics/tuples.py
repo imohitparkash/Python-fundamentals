a8c94957d277c62b8d10eb2e7a53ff917e8cef5e
imohitparkash <imohitparkash1@gmail.com>
2026-09-03 15:04:08 +0530

    add tuples

---
+++ src/GFG_Course/Basics/tuples.py
@@ -0,0 +1,19 @@
+t=(10,20,"hey")
+print(t)
+t=()
+print(t)
+print(type(t))
+t=(10)
+print(type(t))
+t=(10, )
+print(type(t))
+#difference from list 
+#immutable
+#need
+#faster than list
+t=(10,20,30,40,10)
+print(t[2])
+print(t[-1])
+print(t[1:3])
+print(t.count(10))
+print(t.index(10))
